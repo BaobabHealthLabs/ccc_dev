123c8ce6f8c47704d23b6037e004d495ab3ede55
cranberry <manzymkamba@gmail.com>
2016-06-24 15:12:29 +0200

    Next Appointment Encounter

--- public/javascripts/epilepsy.js
+++ public/javascripts/epilepsy.js
@@ -1,141 +1,59 @@
 "use strict"
 
-function loadExposureConditions(){
-
-	var opts = __$("touchscreenInput" + tstCurrentPage).value.split(";"); 
-   
-  	//Exposure Concepts
-	if(opts.indexOf("Smoking") >= 0){
-
-		__$("smoking_date").setAttribute("condition", true)
-
-	} else {
-
-		__$("smoking_date").setAttribute("condition", false)
-
-	}
-
-	if(opts.indexOf("Alcohol") >= 0){
-
-		__$("alcohol_date").setAttribute("condition", true)
-
-	} else {
-
-		__$("alcohol_date").setAttribute("condition", false)
-
-	}
-
-	if(opts.indexOf("Pigs")>=0){
-
-		__$("pigs_date").setAttribute("condition", true)
-
-	}
-	else{
-
-		__$("pigs_date").setAttribute("condition", false)
-
-	}
-
-	if(opts.indexOf("Traditional medicine")>=0){
-
-		__$("traditional_date").setAttribute("condition", true)
-
-	}
-	else{
-
-		__$("traditional_date").setAttribute("condition", false)
-
-	}
-	if(opts.indexOf("Other")>=0){
-
-			  __$("other_exposures_specify").setAttribute("condition",true)
-
-			  __$("other_exposures_date").setAttribute("condition", true)
-
-			
-	}
-	else{
-
-		__$("other_exposures_date").setAttribute("condition", false)
-		__$("other_exposures_specify").setAttribute("condition",false)
-
-
-	}
-
-
-	//Complicatons concepts
-	
-
-}
-
-function loadComplicationsConditions(){
+function loadCheckConditions(){
 
 	var opts = __$("touchscreenInput" + tstCurrentPage).value.split(";");
 
-	if(opts.indexOf("Injuries") >= 0){
+	var all_page_options = ["Smoking","Alcohol","Pigs/pork",
+							"Traditional medicine","Other",
+							"Injuries","Burns","Status Epilepticus","Psychosis","Drug Related"];
+							
 
-		__$("injuries_date").setAttribute("condition", true)
+	for(var i = 0; i < all_page_options.length; i++){
 
-	} else {
+		var element_id = all_page_options[i].trim().toLowerCase().replace("/","_").replace(/\s+/g,"_");
 
-		__$("injuries_date").setAttribute("condition", false)
 
-	}
+		if(opts.indexOf(all_page_options[i]) >= 0){	
 
-	if(opts.indexOf("Burns") >= 0){
 
-		__$("burns_date").setAttribute("condition", true)
+			if (__$(element_id +"_date") !=null && __$(element_id +"_date") !== 'undefined'){
 
-	} else {
+				__$(element_id +"_date").setAttribute("condition", true)
+			}
+			else{
 
-		__$("burns_date").setAttribute("condition", false)
+				for(var j = 0 ; j < 5 ; j++){
 
-	}
+					if (__$(element_id +j) !=null && __$(element_id +j) !== 'undefined'){
 
-	if(opts.indexOf("Status Epilepticus")>=0){
+						__$(element_id +j).setAttribute("condition", true)
 
-		__$("status_epilepticus_date").setAttribute("condition", true)
+					}
 
-	}
-	else{
-
-		__$("status_epilepticus_date").setAttribute("condition", false)
-
-	}
-
-	if(opts.indexOf("Psychosis")>=0){
-
-		__$("psychosis_date").setAttribute("condition", true)
-
-	}
-	else{
-
-		__$("psychosis_date").setAttribute("condition", false)
-
-	}
-	if(opts.indexOf("Drug Related")>=0){
-
-		__$("drug_related_date").setAttribute("condition", true)
-
-	}
-	else{
-
-		__$("drug_related_date").setAttribute("condition", false)
-
-	}
-	if(opts.indexOf("Other")>=0){
-
-			  __$("other_complications_specify").setAttribute("condition",true)
-
-			  __$("other_complications_date").setAttribute("condition", true)
-
+				}
+			}
+		}
+		else{
 			
-	}
-	else{
+			if (__$(element_id +"_date") !=null && __$(element_id +"_date") !== 'undefined'){
 
-		__$("other_complications_date").setAttribute("condition", false)
-		__$("other_complications_specify").setAttribute("condition",false)
+				__$(element_id +"_date").setAttribute("condition", false)
 
+			}
+			else{
 
+				for(var j = 0 ; j < 5 ; j++){
+
+					if (__$(element_id +j) !=null && __$(element_id +j) !== 'undefined'){
+
+						__$(element_id +j).setAttribute("condition", false)
+
+					}
+
+				}
+
+			}
+		}
 	}
 }
