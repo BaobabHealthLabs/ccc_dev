acd5f1c311ba1941c52548a7d2dda8588917b4f9
Chimwemwe Kachaje <chimwemwe.kachaje@baobabhealth.org>
2016-06-30 23:13:55 +0200

    Added logic for dynamically locking configuration files to current IP address

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ var Mutex = require("Mutex");
 var md5 = require("md5");
 var randomstring = require("randomstring");
 
+var ip = require("ip");
+var fs = require("fs");
+
 var mutex = new Mutex("htc_lock");
 
 var url = require("url");
@@ -6162,6 +6165,31 @@ portfinder.basePort = 3015;
 portfinder.getPort(function (err, port) {
 
     server.listen(port, function () {
+
+        var files = [
+                __dirname + "/public/config/dashboard.settings.json",
+                __dirname + "/public/config/landing.settings.json",
+                __dirname + "/public/config/patient.settings.json",
+                __dirname + "/public/config/stock.settings.json",
+                __dirname + "/public/config/user.settings.json"
+        ]
+
+        var address = "http://" + ip.address() + ":" + port;
+
+        for(var i = 0; i < files.length; i++) {
+
+            var filename = files[i];
+
+            var settings = require(filename);
+
+            settings.basePath = address;
+
+            var data = JSON.stringify(settings);
+
+            fs.writeFileSync(filename, data);
+
+        }
+
         console.log("✔ Server running on port %d in %s mode", port, app.get("env"));
     });
 
