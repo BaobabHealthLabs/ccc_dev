e016d64683611fdf3f40f0cc8cf3b99a72a4447c
Chimwemwe Kachaje <chimwemwe.kachaje@baobabhealth.org>
2017-03-08 10:56:49 +0200

    Added ability to show asterisks when typing passwords

--- setup.sh
+++ setup.sh
@@ -35,7 +35,7 @@ getUserPassword()
 {
 	exec 3>&1
 	
-  RETVAL=$(dialog --clear --backtitle "$1" --title "$2" --passwordbox "$3" 16 51 2>&1 1>&3)
+  RETVAL=$(dialog --clear --backtitle "$1" --title "$2" --insecure --passwordbox "$3" 16 51 2>&1 1>&3)
   
   EXIT_CODE=$?
   
